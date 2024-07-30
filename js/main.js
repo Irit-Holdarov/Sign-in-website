35c5e47d73e12964ac21d4074bf413c83026e173
Irit-Holdarov <iritholdarov1233@gmail.com>
2024-07-30 12:28:59 +0300

    Deleting and adding relevant photos and designing the main gallery

--- js/main.js
+++ js/main.js
@@ -1,43 +1,29 @@
+
+
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
   const images = [
-    'img/signin-1.jpeg',
-    'img/signin-2.jpeg',
-    'img/signin-3.jpeg',
-    'img/signin-4.jpeg',
-    'img/signin-5.jpeg',
-    'img/signin-6.jpeg',
-    'img/signin-7.jpeg',
-    'img/signin-8.jpeg',
-    'img/signin-9.jpeg',
-    'img/signin-10.jpeg',
-    'img/signin-11.jpeg',
-    'img/signin-12.jpeg',
-    'img/signin-13.jpeg',
-    'img/signin-14.jpeg',
-    'img/signin-15.jpeg',
-    'img/signin-16.jpeg',
-    'img/signin-17.jpeg',
-    'img/signin-18.jpeg',
-    'img/signin-19.jpeg',
-    'img/signin-20.jpeg',
-    'img/signin-21.jpeg',
-    'img/signin-22.jpeg',
-    'img/signin-23.jpeg'
+    { src: 'img/img-main1.jpeg', position: '0 35%' },
+    { src: 'img/img-main2.jpeg', position: '0 90%' },
+    { src: 'img/img-main3.jpeg', position: '0 72%' },
+    { src: 'img/img-main4.jpeg', position: '0 160%' },
+    { src: 'img/img-main5.jpeg', position: 'center' },
+    { src: 'img/img-main6.jpeg', position: 'center' }
   ];
 
   let currentIndex = 0;
-
   const gallery = document.querySelector('.gallery');
 
   function changeBackgroundImage() {
-    gallery.style.backgroundImage = `linear-gradient(to right bottom, rgba(65, 57, 57, 0.8), rgba(56, 56, 56, 0.5)), url(${images[currentIndex]})`;
+    const image = images[currentIndex];
+    gallery.style.backgroundImage = `linear-gradient(to right bottom, rgba(49, 49, 49, 0.8), rgba(56, 56, 56, 0.5)), url(${image.src})`;
+    gallery.style.backgroundPosition = image.position;
     currentIndex = (currentIndex + 1) % images.length;
   }
 
-  // Change image every 3 seconds
-  setInterval(changeBackgroundImage, 4000);
+  // Change image every 5 seconds (adjust as needed)
+  setInterval(changeBackgroundImage, 3000);
 
   // Initialize with the first image
   changeBackgroundImage();
