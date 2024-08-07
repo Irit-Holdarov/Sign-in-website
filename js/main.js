193d7c2fe4aaa191cdecebcace221aca27950353
Irit-Holdarov <iritholdarov1233@gmail.com>
2024-08-07 13:43:17 +0300

    Added swiper imgs

--- js/main.js
+++ js/main.js
@@ -24,3 +24,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
   changeBackgroundImage()
 })
+
+var TrandingSlider = new Swiper('.tranding-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    strech: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: 'swiper-button-prev'
+  }
+})
