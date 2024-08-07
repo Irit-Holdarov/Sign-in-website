56cf200341235096fa02f3d3728d527b1dd6a199
Irit-Holdarov <iritholdarov1233@gmail.com>
2024-08-07 19:34:28 +0300

    Code optimization of the service section

--- js/main.js
+++ js/main.js
@@ -25,6 +25,59 @@ document.addEventListener('DOMContentLoaded', function () {
   changeBackgroundImage()
 })
 
+const servicesData = [
+  {
+    img: "img/img-service/service-img1.jpeg",
+    alt: "signin1",
+    title: "ניהול ותפעול אירועים",
+    text: "תכנון, ארגון וביצוע מקצועי של כל היבטי האירוע, כולל ניהול לוגיסטי, צוותים ומתחמים כדי להבטיח שהאירוע יתנהל בצורה מושלמת."
+  },
+  {
+    img: "img/img-service/service-img2.jpeg",
+    alt: "signin2",
+    title: "עמדות רישום לאירועים",
+    text: "פתרון קל, מהיר ומסודר לרישום המשתתפים, תוך דגש על מקצועיות ויעילות לחוויית כניסה מהירה ונוחה."
+  },
+  {
+    img: "img/img-service/service-img3.jpeg",
+    alt: "signin3",
+    title: "שירותי דיילות לאירועים",
+    text: "דיילות מנוסות ומקצועיות אשר יעניקו שירות יעיל ואדיב לאורחים שלך."
+  },
+  {
+    img: "img/signin-1.jpeg",
+    alt: "signin4",
+    title: "שמירת חפצים",
+    text: "צוות מיומן לניהול ושמירת חפצים בצורה מקצועית ובטוחה."
+  },
+  {
+    img: "img/img-service/service-img5.jpeg",
+    alt: "signin5",
+    title: "דפי נחיתה",
+    text: "פתרון מותאם אישית ליצירת דפי רישום ייחודיים לאירועים, עם עיצוב גמיש ופונקציונליות גבוהה תוך דגש על חווית משתמש מעולה."
+  }
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+  const servicesContainer = document.querySelector(".services__grid-box");
+
+  servicesData.forEach(service => {
+    const serviceCol = document.createElement("div");
+    serviceCol.classList.add("services__grid-box--col");
+
+    serviceCol.innerHTML = `
+      <div class="services__box">
+        <img src="${service.img}" alt="${service.alt}" class="services__img">
+        <h3 class="services__title">${service.title}</h3>
+        <p class="services__text">${service.text}</p>
+      </div>
+    `;
+
+    servicesContainer.appendChild(serviceCol);
+  });
+});
+
+
 const images = [
   'img/signin-1.jpeg',
   'img/signin-2.jpeg',
