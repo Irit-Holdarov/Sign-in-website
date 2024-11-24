433cd33f80f904bd275f68c691c6bccedd897054
Irit-Holdarov <iritholdarov1233@gmail.com>
2024-11-24 14:30:35 +0200

    File name change from popup to about

--- js/about.js
+++ js/about.js
@@ -1,29 +1,4 @@
 'use strict'
-
-// Get the elements
-const popup = document.querySelector(".popup")
-const trigger = document.querySelector(".about__button-trigger")
-const closeBtn = document.querySelector(".popup-close-btn")
-
-// When the user clicks on the trigger, show the popup
-trigger.addEventListener("click", function () {
-  popup.style.display = "flex"
-})
-
-// When the user clicks on the close button, hide the popup
-closeBtn.addEventListener("click", function () {
-  popup.style.display = "none"
-})
-
-// Close the popup if the user clicks outside the popup content
-window.addEventListener("click", function (event) {
-  if (event.target === popup) {
-    popup.style.display = "none"
-  }
-})
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
@@ -55,3 +30,29 @@ document.addEventListener('DOMContentLoaded', () => {
     observer.observe(aboutSection)
   }
 })
+
+
+//Code to the popup
+// Get the elements
+const popup = document.querySelector(".popup")
+const trigger = document.querySelector(".about__button-trigger")
+const closeBtn = document.querySelector(".popup-close-btn")
+
+// When the user clicks on the trigger, show the popup
+trigger.addEventListener("click", function () {
+  popup.style.display = "flex"
+})
+
+// When the user clicks on the close button, hide the popup
+closeBtn.addEventListener("click", function () {
+  popup.style.display = "none"
+})
+
+// Close the popup if the user clicks outside the popup content
+window.addEventListener("click", function (event) {
+  if (event.target === popup) {
+    popup.style.display = "none"
+  }
+})
+
+
